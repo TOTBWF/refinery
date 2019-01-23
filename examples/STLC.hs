3c03a066e8763f4d8e8a935d0c200402e00590d2
Reed Mullanix <reedmullanix@gmail.com>
2019-01-22 22:46:54 -0800

    Update to the new ProofStateT

--- examples/STLC.hs
+++ examples/STLC.hs
@@ -1,95 +1,58 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE LambdaCase #-}
 module STLC where
 
-import GHC.Generics
-import Data.Typeable
-import Data.List (find)
-
-import Data.Map (Map)
-import qualified Data.Map as Map
-
 import Control.Monad.Except
 
+import Refinery.Tactic
 
-import Unbound.Generics.LocallyNameless
-
-import Refinery.Hole
-import Refinery.Telescope (Telescope, (@>))
-import qualified Refinery.Telescope as Tl
-import qualified Refinery.Tactic as T
-import Refinery.Extract
-
-type Var = Name Term
+type Var = String
 
 data Term
-    = Var Var
-    | Hole (Ignore (MetaVar Term))
-    | Lam (Bind Var Term)
-    | App Term Term
-    deriving (Show, Typeable, Generic)
-
-instance Alpha Term
-instance Subst Term Term where
-    isvar (Var x) = Just $ SubstName x
-
-lam :: Var -> Term -> Term
-lam x t = Lam (bind x t)
-
-metaSubst' :: (LFresh m) => MetaVar Term -> Term -> Term -> m Term
-metaSubst' x e = \case
-  Hole (unignore -> x') | x == x' -> return e
-  Lam bnd -> lunbind bnd $ \(x',b) -> lam x' <$> metaSubst' x e b
-  App f a -> App <$> metaSubst' x e f <*> metaSubst' x e a
-  t -> return t
-
-metaSubsts' :: (LFresh m) => [(MetaVar Term, Term)] -> Term -> m Term
-metaSubsts' xs = \case
-  Hole (unignore -> x') -> case find (\(x,e) -> x == x') xs of
-    Just (_, e) -> return e
-    Nothing -> return $ Hole $ ignore x'
-  Lam bnd -> lunbind bnd $ \(x',b) -> lam x' <$> metaSubsts' xs b
-  App f a -> App <$> metaSubsts' xs f <*> metaSubsts' xs a
-  t -> return t
-
-
-instance MetaSubst Term Term where
-  metaSubst x e t = runLFreshM $ metaSubst' x e t
-  metaSubsts xs t = runLFreshM $ metaSubsts' xs t
-
-type TVar = Name Type
-
-data Type
-  = TVar TVar
-  | Type :-> Type
-  deriving (Show, Typeable, Generic)
-
-instance Alpha Type
-instance Subst Type Type where
-  isvar (TVar a) = Just $ SubstName a
-  isvar _ = Nothing
-
-data Judgement = J (Map Var Type) Type 
+  = Var Var
+  | Hole
+  | Lam Var Term
   deriving (Show)
 
-type Tactic = T.Tactic Term Judgement (FreshMT (Except ()))
+instance Extract Term where
+  hole = Hole
 
-intro :: Tactic
-intro = T.tactic $ \case
-  J ctx (t1 :-> t2) -> do
-    x <- fresh $ s2n "x"
-    mx <- T.subgoal (J (Map.insert x t1 ctx) t2)
-    return $ lam x (Hole $ ignore mx)
+data Type
+  = TVar Var
+  | TArrow Type Type
+  deriving (Show, Eq)
 
-assumption :: Tactic
-assumption = T.tactic $ \(J ctx t) -> 
-  case Map.keys $ Map.filter (aeq t) ctx of
-    (x:_) -> return $ Var x
-    [] -> throwError ()
+data Judgement = Judgement [(Var, Type)] Type
+  deriving (Show)
 
-goal :: Type
-goal = let a = s2n "a" in (TVar a) :-> (TVar a)
+data TacticError
+  = UndefinedHypothesis Var
+  | GoalMismatch String Type
+  | UnsolvedSubgoals [Judgement]
+  deriving (Show)
 
-tac = intro <> assumption
+exact :: (MonadError TacticError m) => Var -> TacticT Judgement Term m ()
+exact s = rule $ \(Judgement hy g) ->
+  case lookup s hy of
+    Just t -> if t == g then return (Var s) else throwError (GoalMismatch "exact" t)
+    Nothing -> throwError $ UndefinedHypothesis s
+
+intro :: (MonadError TacticError m) => Var -> TacticT Judgement Term m ()
+intro s = rule $ \(Judgement hy g) ->
+  case g of
+    (TArrow a b) -> Lam s <$> subgoal (Judgement ((s,a):hy) b)
+    t -> throwError $ GoalMismatch "intro" t
+
+runTactic :: Type -> TacticT Judgement Term (Except TacticError) () -> Either TacticError Term
+runTactic ty tac = runExcept $ runTacticT tac (Judgement [] ty) >>= \case
+  (t, []) -> return t
+  (_, sg) -> throwError $ UnsolvedSubgoals sg
+
+example = runTactic (TArrow (TVar "a") (TArrow (TVar "b") (TVar "a"))) $ do
+  intro "x"
+  intro "y"
+  exact "x"
