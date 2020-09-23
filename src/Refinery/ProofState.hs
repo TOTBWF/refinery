f4e7931b9e5574e929e0bbcd9265f842ed572003
Sandy Maguire <sandy@sandymaguire.me>
2020-09-23 10:12:39 -0700

    MonadThrow/MonadIO; remove dead code

--- src/Refinery/ProofState.hs
+++ src/Refinery/ProofState.hs
@@ -113,78 +113,15 @@ collect (Alt p1 p2)      = do
 collect Empty            = pure $ Right []
 collect (Failure err)    = pure $ Left err
 collect (Axiom ext)      = pure $ Right [(ext, [])]
--- instance MonadTrans (ProofStateT ext err) where
---     lift m = Effect $ fmap pure m
-    -- (Pure a)      >>= f = _
-      -- where
-      --   go :: ProofStateT ext err m (a -> b) -> ProofStateT ext err m b
-      --   go (Subgoal goal k) = _k
-      --   -- go (M m) = M (fmap go _)
-      --       -- M (fmap go m)
-      --   go (Failure err) = Failure err
-      --   go (Pure f) = fmap f px
 
-    -- instance (Monad m) => Monad (ProofStateT ext err m) where
-    -- return = pure
+instance (MonadIO m) => MonadIO (ProofStateT ext err m) where
+  liftIO = lift . liftIO
 
--- newtype ProofStateT ext m jdg = ProofStateT { unProofStateT :: Client jdg ext m ext }
-
--- instance (Monad m) => Functor (ProofStateT ext m) where
---   fmap f (ProofStateT p) = ProofStateT $ (request . f) >\\ p
-
--- instance (Monad m) => Applicative (ProofStateT ext m) where
---   pure a = ProofStateT $ request a
---   (ProofStateT pf) <*> (ProofStateT pa) = ProofStateT $ (\f -> (request . f) >\\ pa) >\\ pf
-
--- instance (Monad m) => Monad (ProofStateT ext m) where
---   return = pure
---   (ProofStateT p) >>= k = ProofStateT $ (unProofStateT . k) >\\ p
-
--- instance MonadTrans (ProofStateT ext) where
---   lift m = ProofStateT $ request =<< (lift m)
-
--- instance (MonadIO m) => MonadIO (ProofStateT ext m) where
---   liftIO m = ProofStateT $ request =<< (liftIO m)
-
--- instance (MonadError err m) => MonadError err (ProofStateT ext m) where
---   throwError e = ProofStateT $ lift $ throwError e
---   catchError (ProofStateT m) h = ProofStateT $ catchError m (unProofStateT . h)
-
--- instance (MonadPlus m) => Alternative (ProofStateT ext m) where
---   empty = lift empty
---   (ProofStateT p1) <|> (ProofStateT p2) = ProofStateT (go p1)
---     where
---       go (Request a' fa) = Request a' (go . fa)
---       go (Respond b fb') = Respond b (go . fb')
---       go (Pure r) = M (pure (Pure r) <|> pure p2)
---       go (M m) = M (fmap go m)
-
--- -- instance (MonadPlus m) => MonadLogic (ProofStateT ext m) where
--- --     msplit (ProofStateT p) = ProofStateT (go p)
--- --     -- (_ >\\ p)
--- --       where
--- --         go :: Client jdg ext m ext -> Client (Maybe (jdg, ProofStateT ext m jdg)) ext m ext
--- --         go (Request a' fa) = Request (Just (a', ProofStateT $ _)) (go . fa)
--- --         go (Respond b fb') = Respond b (go . fb')
--- --         go (Pure r) = Pure r
--- --         go (M m) = M (fmap go m)
---     -- msplit (ProofStateT (Respond a
---     -- msplit (ProofStateT (Pure r)) = ProofStateT _pure
---     -- msplit (ProofStateT (M m)) = _h
-
--- instance (MonadPlus m) => MonadPlus (ProofStateT ext m) where
---   mzero = empty
---   mplus = (<|>)
-
--- instance (MonadThrow m) => MonadThrow (ProofStateT ext m) where
---   throwM e = ProofStateT $ lift $ throwM e
+instance (MonadThrow m) => MonadThrow (ProofStateT ext err m) where
+  throwM = lift . throwM
 
 -- instance (MonadCatch m) => MonadCatch (ProofStateT ext m) where
 --   catch (ProofStateT m) h = ProofStateT $ catch m (unProofStateT . h)
-
--- instance (MonadReader env m) => MonadReader env (ProofStateT ext m) where
---   ask = lift ask
---   local f (ProofStateT m) = ProofStateT $ local f m
 
 instance (Monad m) => MonadError err (ProofStateT ext err m) where
     throwError = Failure
